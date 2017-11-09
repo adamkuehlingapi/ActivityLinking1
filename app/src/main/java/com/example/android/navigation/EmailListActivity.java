4d8aa823f8191f8a68b48ee7274a4e03c65a02b4
adamkuehling <adamkuehlingapi@gmail.com>
2017-11-09 12:26:12 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/navigation/EmailListActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.navigation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class EmailListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_list);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(MainActivity.NAME_KEY);  //gets the IntentExtra passed from Main Activity
+
+        EditText et = (EditText) findViewById(R.id.name_text); //edits and set info that was passed. (propulate forms)
+        et.setText(name);
+    }
+
+
+
+
+
+}
